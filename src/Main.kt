0c9f1ec8b1e23eca340b790203158f8344274099
Steve Copley <steve.copley@waimea.school.nz>
2025-02-13 12:48:58 +1300

    First go at the game

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,7 @@
 
 fun main() {
     println("---------------------------")
-    println(" Guessing Game 5 Million")
+    println("  Guessing Game 5 Million")
     println("---------------------------")
     println()
 
@@ -17,11 +17,30 @@ fun main() {
     // Main loop
     while (true) {
         // Ask the user for a guess
+        val guess = readln().toIntOrNull()
 
-        // Check if it's correct or not
+        // Did the user enter a non-number?
+        if (guess == null) {
+            println("Enter a number!")
+            continue  // Start loop again
+        }
 
-        // Break out of loop if correct
+        // Must be a number, so let's check it
+
+        // Check if it's correct
+        if (guess == numToGuess) {
+            println("Well done!")
+            break   // Correct, so leave the loop
+        }
 
         // Otherwise give high/lower feedback
+        if (guess > numToGuess) {
+            println("You guessed too HIGH!")
+        }
+        else {
+            println("You guessed too LOW!")
+        }
     }
+
+    // Show final feedback
 }
