cc914208a1b8b913d6d28c36b92a86b7c76fe80b
Steve Copley <steve.copley@waimea.school.nz>
2025-02-17 11:00:24 +1300

    Using functions for inputs

--- src/Main.kt
+++ src/Main.kt
@@ -9,38 +9,77 @@ fun main() {
     println("---------------------------")
     println()
 
+    val playerName = getString("Enter your name: ")
+    println("Nice to meet you, $playerName")
+
     val numToGuess = (1..100).random()
 
     println("I'm thinking of a number from 1 to 100")
     println("Can you guess it?")
+    println()
+
+    var guesses = 0
 
     // Main loop
     while (true) {
         // Ask the user for a guess
-        val guess = readln().toIntOrNull()
+        val guess = getInt("Enter your guess: ")
 
-        // Did the user enter a non-number?
-        if (guess == null) {
-            println("Enter a number!")
-            continue  // Start loop again
-        }
-
-        // Must be a number, so let's check it
+        // They've made a guess, so keep a count
+        guesses++
 
         // Check if it's correct
         if (guess == numToGuess) {
-            println("Well done!")
+            println("Well done! The number was $guess!")
             break   // Correct, so leave the loop
         }
 
         // Otherwise give high/lower feedback
         if (guess > numToGuess) {
-            println("You guessed too HIGH!")
+            println("$guess is too HIGH!")
         }
         else {
-            println("You guessed too LOW!")
+            println("$guess is too LOW!")
         }
     }
 
     // Show final feedback
+    println("You too $guesses guesses")
+}
+
+
+/**
+ * Function to get a string from the user
+ *
+ * parameters:
+ * - prompt - Text to show the user
+ * returns:
+ * - String that the user types
+ */
+fun getString(prompt: String): String {
+    var userInput: String
+
+    while(true) {
+        print(prompt)
+
+        userInput = readln()
+        if (userInput.isNotBlank()) break
+    }
+
+    return userInput
+}
+
+/**
+ * Function to get an Int from the user
+ */
+fun getInt(prompt: String): Int {
+    var intValue: Int?
+
+    while(true) {
+        val userInput = getString(prompt)
+        intValue = userInput.toIntOrNull()
+        if (intValue != null) break
+    }
+
+    return intValue!!
 }
